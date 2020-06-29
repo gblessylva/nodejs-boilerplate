95a2dba019c8b9dc0952bbb97b3dff434d259a2e
Olaide <olaidemmanuel1@gmail.com>
2020-06-29 09:12:41 +0100

    Installed color npm package and add the config file to root folder

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const cors = require ('cors')
-const mongoose = require('mongoose')
-const routes = require('./routes/index')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const colors = require('colors');
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
+const app = express();
+// This is the link to the db file in config
+// const connectDB = require('../config/db');
 
-require('dotenv').config()
+//This is will call the DB which will connect to the MongoDB
+// connectDB();
+
+require('dotenv').config();
+// dotenv.config({
+//     path: '../config/config.env'
+// });
 app.use(express.json());
-app.use(cors())
-app.use(routes)
+app.use(cors());
+app.use(routes);
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db = mongoose.connection
-db.on('error', error=>console.log(error))
-db.once('open', ()=>console.log("connected to database"))
+// const db = mongoose.connection
+// db.on('error', error => console.log(error));
+// db.once('open', () => console.log("connected to database").yellow.bold);
 
-app.listen(process.env.PORT, ()=>{
-  console.log('Api is live')
-})
+
+const PORT = 5000;
+app.listen(PORT,
+    console.log(`Server is running on port ${PORT}`.green.bold));
